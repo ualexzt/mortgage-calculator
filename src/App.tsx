c1a14ea6c76413912b60f1a81b9914620711d37e
AlexUa <ualexzt@gmail.com>
2022-04-13 14:52:55 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import { Container } from '@mui/material';
+import BankList from './components/bankList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container
+        maxWidth="lg"
+        sx={{ display: 'flex', flexWrap: 'wrap', mt: 2, justifyContent: 'center', gap: 5 }}
+      >
+        <BankList />
+      </Container>
+    </>
   );
 }
 
